16a26a513a73fcf47c6cf428fdbe96ab8e619a94
email:samunderwood1989@gmail.com <samunderwood1989@gmail.com>
2024-07-14 11:15:54 +1000

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+const generateTarget = () => Math.floor(Math.random() * 10);
+
+const compareGuesses = (userGuess, computerGuess, targetNumber) => {
+  // Calculate the absolute differences
+  const userDifference = Math.abs(userGuess - targetNumber);
+  const computerDifference = Math.abs(computerGuess - targetNumber);
+
+  // Determine the winner
+  if (userDifference <= computerDifference) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const updateScore = (winner) => {
+  if (winner === 'human') {
+    humanScore += 1;
+  } else if (winner === 'computer') {
+    computerScore += 1;
+  }
+};
+
+const advanceRound = () => {
+  currentRoundNumber = +1;
+};
